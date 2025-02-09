e9fbe2a54038a457d0a4b8f0817a458ac4155af0
yumna1623 <yumnanasir1623@gmail.com>
2025-02-09 19:07:05 +0500

    theme switcher

---
+++ 08Theme_Switcher/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "./context/Theme";
+import "./index.css"; // Ensure Tailwind is imported
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+);
